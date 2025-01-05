38b8ab4813635b5909b73e26952ef79b48a331f4
Jannixen <jannixen@gmail.com>
2025-01-05 18:04:04 +0100

    Add conversational memory buffer to the retrieval

--- database_utils.py
+++ database_utils.py
@@ -2,6 +2,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 from qdrant_client import QdrantClient
 from qdrant_client.models import PointStruct, VectorParams, Distance
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 import uuid
 import yake
 
@@ -14,69 +15,76 @@ def split_texts_for_db(text):
     return c_splitter.split_text(text)
 
 def calculate_embedding(input_data):
-    # Load the model
-    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    embeddings = FastEmbedEmbeddings()
 
     if isinstance(input_data, str):
         # Encode a single sentence
-        return model.encode(input_data)
+        return embeddings.embed_documents(input_data)[0]
     elif isinstance(input_data, list):
         # Encode a list of sentences
-        return [model.encode(sentence) for sentence in input_data]
+        return [embeddings.embed_documents(sentence)[0] for sentence in input_data]
     else:
         raise ValueError("Input must be a string or a list of strings")
 
-def add_article_to_qdrant(row, qdrant_key, qdrant_url):
+def add_article_to_qdrant(row, qdrant_key, qdrant_url, collection_name = "bbc_news_articles"):
     client = QdrantClient(url=qdrant_url, api_key=qdrant_key)
-    metadata = {'summary': row['summary'], 'label': row['labels'], 'keywords': row['keywords'], 'source': row['data_source']}
+    metadata = { 'label': row['labels'], 'keywords': row['keywords'], 'source': row['data_source']}
     
-    for embedding in row['embeddings']:
+    for embedding, text in zip(row['embeddings'], row['splitted_text']):
         
         vector = embedding
     
-        if not client.collection_exists("bbc_news_articles"):
+        if not client.collection_exists(collection_name):
             client.create_collection(
-                collection_name="bbc_news_articles",
+                collection_name=collection_name,
                 vectors_config=VectorParams(size=384, distance=Distance.COSINE),
             )
     
         # Add data to collection
         client.upsert(
-            collection_name="bbc_news_articles",
+            collection_name=collection_name,
             points=[
                 PointStruct(
                     id=str(uuid.uuid4()),
                     vector=vector,
-                    payload=metadata
+                    payload={
+                    "page_content": text,  # Include the text content as `page_content`
+                    **metadata  # Add additional metadata
+                }
                 )
             ]
         )
 
-def add_articles_to_qdrant(df, qdrant_key, qdrant_url):
+def add_articles_to_qdrant(df, qdrant_key, qdrant_url, collection_name = "bbc_news_articles"):
     
     client = QdrantClient(url=qdrant_url, api_key=qdrant_key)
 
-    if not client.collection_exists("bbc_news_articles"):
+    if not client.collection_exists(collection_name):
         client.create_collection(
-            collection_name="bbc_news_articles",
+            collection_name=collection_name,
             vectors_config=VectorParams(size=384, distance=Distance.COSINE),
         )
     
 
     for _, row in df.iterrows():
-        metadata = {'summary': row['summary'], 'label': row['labels'], 'keywords': row['keywords'], 'source': row['data_source']}
+        metadata = {'label': row['labels'], 'keywords': row['keywords'], 'source': row['data_source']}
         
-        for embedding in row['embeddings']:
-        
+        for embedding, text in zip(row['embeddings'], row['splitted_text']):
+
             vector = embedding
+            
             # Add data to collection
             client.upsert(
-                collection_name="bbc_news_articles",
+                collection_name=collection_name,
                 points=[
                     PointStruct(
                         id=str(uuid.uuid4()),
                         vector=vector,
-                        payload=metadata
+                        payload={
+                    "page_content": text,  # Include the text content as `page_content`
+                    **metadata  # Add additional metadata
+                }
                     )
                 ]
             )
